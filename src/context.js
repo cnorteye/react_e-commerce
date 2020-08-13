3ab44d99d8bd34d7bd58a40120cc040788bc9e8c
Comfort <65386761+cnorteye@users.noreply.github.com>
2020-08-13 01:02:01 -0700

    inital work on e-commerce app with react.

---
+++ src/context.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {storeProducts, detailProduct} from './data';
+
+
+const ProductContext = React.createContext();
+//Provider - provide all information for our application
+//Consumer - use the information 
+
+ class ProductProvider extends Component {
+     state={
+         products:[], 
+         detailProduct:detailProduct
+     };
+     componentDidMount(){
+         this.setProducts();
+     }
+     setProducts =() =>{
+         let tempProducts =[];
+         storeProducts.forEach(item =>{
+             const singleItem = {...item};
+             tempProducts = [...tempProducts, singleItem];
+
+         })
+         this.setState(()=>{
+             return{products:tempProducts}
+         });
+     };
+
+     getItem = (id) => {
+         const product = this.state.products.find(item => item.id ===id);
+         return product;
+     };
+
+     handleDetail = (id) => {
+         const product = this.getItem(id);
+         this.setState(() =>{
+             return {detailProduct:product}
+         })
+         
+     }; 
+     addToCart = (id) =>{
+        console.log(`hello from add to cart.id is ' ${id}`);
+    }
+    render() {
+        return(
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart:this.addToCart,
+            }}
+            >
+
+                {this.props.children}
+            </ProductContext.Provider>
+        );
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer}
